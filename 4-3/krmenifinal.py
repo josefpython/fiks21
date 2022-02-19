10b95528d53e349fc687be9db8adc08c32114098
josf <jayiikdev@gmail.com>
2022-02-19 21:56:30 +0100

    push b4 cleanup

--- 4-3/krmenifinal.py
+++ 4-3/krmenifinal.py
@@ -1,35 +1,8 @@
-from itertools import count
+from importlib.resources import path
 import sys
 from collections import defaultdict
 from treelib import Tree
 import time
-
-def makeTree(root, graph):
-    tree = Tree()
-    tree.create_node(root,root)
-    g = graph
-
-    q = []
-    v = set()
-    
-    q.append(root)
-    
-    while q:
-        c = q.pop(0)
-        for child in g[c]:
-            if not (child in v):
-                tree.create_node(child, child, parent=c)
-                q.append(child)
-        v.add(c)
-    return tree
-
-def moveRoot(root, tree: Tree):
-    givenTree = tree
-    newTree = tree.remove_subtree(root)
-    
-    newTree.paste(root,givenTree)
-    return newTree
-    
 
 with sys.stdin as f:
 
@@ -96,7 +69,11 @@ with sys.stdin as f:
             res = str(exp-ans)
 
 
-        else:
+        elif amnt == 100 and False:
+
+            # [ [depth,[ pathToRoot ]], ... ]
+
+            iterList = []
 
             st = time.time()
 
@@ -104,39 +81,103 @@ with sys.stdin as f:
             #Tree But With Only Relevant Nodes
             
             for nodee in nds:
-                prev = None
-                for tray in tree.rsearch(nodee):
-                    if prev != None:
-                        tbworl[prev].add(tray)
-                        tbworl[tray].add(prev)
-                    prev = tray
-            # CORRECT ^^^
-            ks = set(tbworl.keys())
-            bestWeight = exp
-            root = 1
-            #t=makeTree(root,tbworl)
-            while True:
-                childChanged = False
-                ar = set([g.identifier for g in tree.children(root)]).intersection(ks)
-                try:
-                    
-                    for child in ar:
-                        t = makeTree(child,tbworl)
-                        xy=0
-                        for n in nds:
-                            xy+=t.depth(n)
-                        if xy < bestWeight:
-                            bestWeight = xy
-                            root = child
-                            childChanged = True
-                        
-                    if not childChanged:
-                        break
-                except:
-                    break
+                iterList.append([tree.depth(nodee),[]])
+                pth = [x for x in tree.rsearch(nodee)][::-1][1:]
+                iterList[-1][1] = pth
 
             print(f"{time.time()-st}")
-            res = str(exp-bestWeight)
 
+            newBest = iterList
+            bestd = exp
+            changed = True
+            while changed:
+                changed = False
+                bestList = newBest
+                for i in bestList:
+                    try:
+                        chosen = i[1][0]
+                    except:
+                        pass
+                    iLkeep = bestList
+                    l = 0 
+                    for x in iLkeep:
+                        try:
+                            if x[1][0] == chosen:
+                                x[1].pop(0)
+                                x[0] -= 1
+                            else:
+                                x[1].insert(0,chosen)
+                                x[0] += 1
+                            #print(iLkeep)
+                        except:
+                            x[1].insert(0,chosen)
+                            x[0] += 1
+                        
+                    d = sum([a[0] for a in iLkeep])
+                    if bestd > d:
+                        changed = True
+                        newBest = iLkeep
+                        bestd = d
+                if amnt > 110:
+                    print("deeper..")
+
+        else:
+            # {depth:{node:amnt}, ..}
+
+            iterList = {}
+
+            st = time.time()
+
+            tbworl = defaultdict(set)
+            #Tree But With Only Relevant Nodes
+            
+            for nodee in nds:
+                
+                pth = [x for x in tree.rsearch(nodee)][::-1][:-1]
+                
+                for d,p in enumerate(pth):
+                    try:
+                        iterList[d][p] +=1 
+                    except KeyError:
+                        try:
+                            iterList[d][p] = 1
+                        except KeyError:
+                            iterList.update({d:{p:1}})
+                  
+
+            keeper = 0
+            bestNode = 1
+
+            for b,i in iterList.items():
+            #iterate through every level
+            #change to while loop so values can mutate I guess idk
+
+                ch = False
+                #change state to false
+
+                for x in i.keys():
+                    #iterate through every potential w
+
+                    score = keeper
+                   
+                    for y in i.items():
+
+                        if y[0] == x:
+                            score -= y[1]
+                        else:
+                            score += y[1]
+                    #count it up  for the dub n shit 
+
+                    if score < keeper:
+                        ch = True
+                        keeper = score
+                        bestNode = x
+                
+                if not ch:
+                    break
+
+            res = str(-keeper)
+            print(f"{time.time()-st}")
+            
         with open("output.txt", "a") as ft:
             ft.write("\n"+res)
